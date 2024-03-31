5037a1eb6c93e21737071fa75c0d3ab087c48f85
Muhammad32130 <askari.muhammad321@gmail.com>
2024-03-30 22:04:26 -0700

    Filter features update for better UX experience.

--- src/Pages/Search.jsx
+++ src/Pages/Search.jsx
@@ -18,7 +18,6 @@ function Search() {
         })
         .then(parsedData => {
             setData(parsedData.slice(0, 9)); 
-            console.log(parsedData); 
         })
         .catch(error => {
             console.error('Error fetching data:', error);
@@ -51,13 +50,18 @@ function Search() {
       <div>
        
         
-      <div onClick={()=>{setfilter(true)}} className='sm:absolute 2xl:hidden z-20 w-[100%]'>
-      Filters
-      <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M3.9 54.9C10.5 40.9 24.5 32 40 32H472c15.5 0 29.5 8.9 36.1 22.9s4.6 30.5-5.2 42.5L320 320.9V448c0 12.1-6.8 23.2-17.7 28.6s-23.8 4.3-33.5-3l-64-48c-8.1-6-12.8-15.5-12.8-25.6V320.9L9 97.3C-.7 85.4-2.8 68.8 3.9 54.9z"/></svg>
+      <div onClick={()=>{setfilter(true)}} className='text-black absolute top-20 left-10 cursor-pointer '>
+        <div className='flex items-center justify-center'>
+        <svg className='text-blue-500 mr-2' xmlns="http://www.w3.org/2000/svg" height="1.5em" viewBox="0 0 512 512"><path fill="#0059ff" d="M3.9 54.9C10.5 40.9 24.5 32 40 32H472c15.5 0 29.5 8.9 36.1 22.9s4.6 30.5-5.2 42.5L320 320.9V448c0 12.1-6.8 23.2-17.7 28.6s-23.8 4.3-33.5-3l-64-48c-8.1-6-12.8-15.5-12.8-25.6V320.9L9 97.3C-.7 85.4-2.8 68.8 3.9 54.9z"/></svg>
+<h2 className='text-[24px] text-[#0059ff]'>
+
+      Filters 
+</h2>
       
+        </div>
       </div>
-    <div className={`flex ${filter ? 'translate-x' : "translate-x-[-100%]"} transition-all sm:bg-[#e9e6e6]  border flex-col fixed h-[100%]  justify-evenly items-center`}>
-      <div onClick={()=>{setfilter(false)}} className='sm:absolute top-[2%] 2xl:hidden right-[8%]'>
+    <div className={`flex ${filter ? 'translate-x' : "translate-x-[-100%]"} transition-all sm:bg-[#e9e6e6] w-[15%] border flex-col fixed h-[100%]  justify-evenly items-center`}>
+      <div onClick={()=>{setfilter(false)}} className='absolute top-20 right-10 hover:cursor-pointer'>
       <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><path d="M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8 9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z"/></svg>
       </div>
         <div className='mx-6 sm:mt-20'>
@@ -107,6 +111,9 @@ MPG Range:
   <FormControlLabel control={<Switch defaultChecked />} label="Manual" />
   <FormControlLabel  control={<Switch />} label="Automatic" />
 </FormGroup>
+    </div>
+    <div onClick={()=>{setfilter(false)}} className={`${!filter && "hidden"} transition-all w-[85%] bg-[rgba(60,60,60,0.3)] fixed right-0 h-[100vh] `}>
+
     </div>
     <div className='flex justify-center pt-[100px] items-center flex-wrap mx-4 max'>
 {cars && cars.map((items)=>{
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,60 +1,49 @@
-import { Skeleton } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { Skeleton } from "@mui/material";
+import { Link } from "react-router-dom";
 
-function Card({items}) {
- 
-    const calculateRentalPrice = (items) => {
+function Card({ items }) {
+  const calculateRentalPrice = (items) => {
+    const basePrice = 50;
+    let priceMultiplier = 1.5;
+    if (items.combination_mpg < 15) {
+      priceMultiplier = 8;
+    }
+    const rentalPrice = basePrice + priceMultiplier * items?.combination_mpg;
 
-        const basePrice = 50;
-        let priceMultiplier = 1.5; 
-        if(items.combination_mpg < 15){
-            priceMultiplier = 8
-        }
-        const rentalPrice = basePrice + (priceMultiplier * items?.combination_mpg);
-
-        return rentalPrice.toFixed(2);
-      };
-      const rentalPrice = calculateRentalPrice(items)
+    return rentalPrice.toFixed(2);
+  };
+  const rentalPrice = calculateRentalPrice(items);
 
   return (
-    <div className="w-[calc(100%/3)] mx-4  max-h-[464px] sm:w-[85%] xl:w-[60%] 2xl:w-[40%] 3xl:w-[30%] border rounded my-4 flex flex-col items-center ">
-    <div className="w-[90%] mt-10 object-contain">
-        {items.imageUrl ?
-            <img className='h-[326px] object-cover	' src={items.imageUrl} alt="" />
-: <Skeleton width={550} height={300} variant='rectangular'></Skeleton>
-        }
+    <div className="w-[calc(100%/3)] mx-2  max-h-[464px] sm:w-[85%] xl:w-[60%] 2xl:w-[40%] 3xl:w-[30%]  rounded my-4 flex flex-col items-center ">
+      <div className="w-[90%] mt-10 object-contain">
+        {items.imageUrl ? (
+          <img className="h-[326px] object-cover	" src={items.imageUrl} alt="" />
+          ) : (
+            <Skeleton width={550} height={300} variant="rectangular"></Skeleton>
+            )}
+      </div>
+      <div className="w-[90%] flex justify-between">
+        <div>
+          <h1 className="mt-4">{items.name}</h1>
+          <h1 className="mt-4">Fuel Type: {items.fuel_type?.toUpperCase()}</h1>
+        </div>
+        <div className="mt-4 text-center">
+          <h1>
+            Rental Price: $
+            {Math.floor(parseFloat(items.price.replace(/[$,]/g, "")) / 365) +
+              ".99"}
+          </h1>
+          <Link to={`/car/${items._id}`}>
+            <button className="bg-[#D87F5A] my-2 py-2 px-4 hover:bg-[green] transition-all text-[white] rounded text-center">
+              View Deal
+            </button>
+          </Link>
+   
+        </div>
+      </div>
     </div>
-<div className='w-[90%] flex justify-between'>
-    <div >
-
-    <h1 className='mt-4'>
-        {items.name}
-    </h1>
-    <h1 className='mt-4'>
-       Fuel Type: {items.fuel_type?.toUpperCase()}
-    </h1>
-    
-    </div>
-    <div className='mt-4 text-center'>
-        <h1>
-        Rental Price: ${Math.floor(parseFloat(items.price.replace(/[$,]/g, "")) / 365)+".99"}
-        </h1>
-        <Link to={`/car/${items._id}`}>
-        <button className='bg-[#D87F5A] my-2 py-2 px-4 hover:bg-[green] transition-all text-[white] rounded text-center'>
-            View Deal 
-        </button>
-        </Link>
-    </div>
-</div>
-        
-    </div>
-  )
+  );
 }
 
-export default Card
-
-
-
-
-
-
+export default Card;
